6a71ab66bb92a45179e2f57663f0426a04449b9f
ShinjiSSato <shinjisato69@proton.me>
2025-02-16 15:37:16 -0300

    Adding column and same others configurations. And re-indenting all of the files.

--- lua/shinjissato/lazy/colorscheme.lua
+++ lua/shinjissato/lazy/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
---	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
---	{
---  "folke/tokyonight.nvim",
---  lazy = false,
---  priority = 1000,
---  opts = {},
---	}
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-}
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...}
+  --	{
+    --  "folke/tokyonight.nvim",
+    --  lazy = false,
+    --  priority = 1000,
+    --  opts = {},
+    --	}
+    --	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  }
