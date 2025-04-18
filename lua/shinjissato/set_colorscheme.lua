74527f143b930bf5f4ba8a73e16de886dc8f009e
ShinjiSSato <shinjisato69@proton.me>
2025-04-18 11:15:17 -0300

    Update colorscheme for tokyonight-day

--- lua/shinjissato/set_colorscheme.lua
+++ lua/shinjissato/set_colorscheme.lua
@@ -1,11 +1,12 @@
 -- Gruvbox colorscheme
 
- vim.o.background = "light" -- or "dark" for dark mode
+-- vim.o.background = "light" -- or "dark" for dark mode
 -- vim.o.background = "dark" -- or "light" for light mode
- vim.cmd([[colorscheme gruvbox]])
+-- vim.cmd([[colorscheme gruvbox]])
 
 -- Tokyonight colorscheme
 -- vim.cmd([[colorscheme tokyonight-storm]])
+   vim.cmd([[colorscheme tokyonight-day]])
 
 -- Catpuccin colorscheme
 --vim.cmd.colorscheme "catppuccin-latte"
